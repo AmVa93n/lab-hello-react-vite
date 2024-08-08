88f14b28f5afc5489269b6282ef966d5fe110abc
amirvaknin <amir6661@gmail.com>
2024-08-08 10:58:44 +0100

    lab done

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import logo from './assets/ironhack-logo-xs.png';
+import menu from './assets/menu-top-xs.png'
+import feature1 from './assets/icon1.png'
+import feature2 from './assets/icon2.png'
+import feature3 from './assets/icon3.png'
+import feature4 from './assets/icon4.png'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App">
+      <div className='navbar'>
+        <img src={logo} alt='logo' style={{marginRight: 'auto'}}></img>
+        <img src={menu} alt='menu' style={{marginLeft: 'auto'}}></img>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className='main'>
+        <h1>Say hello to ReactJS</h1>
+        <p>You will learn how to use the most popular frontend library, and become a super Ninja developer.</p>
+        <button>Awesome!</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <div className='features'>
+        <div className='feature'>
+          <img src={feature1}></img>
+          <h2>Declarative</h2>
+          <p>React makes it painless to create interactive UIs.</p>
+        </div>
+        <div className='feature'>
+          <img src={feature2}></img>
+          <h2>Components</h2>
+          <p>Build encapsulated components that manage their state.</p>
+        </div>
+        <div className='feature'>
+          <img src={feature3}></img>
+          <h2>Single-Way</h2>
+          <p>A set of immutable values are passed to the components</p>
+        </div>
+        <div className='feature'>
+          <img src={feature4}></img>
+          <h2>JSX</h2>
+          <p>Statically-typed. designed to run on modern broswers.</p>
+        </div>
+      </div>
+    </div>
+  );
+
 }
 
 export default App
